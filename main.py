fd74379053a866daf47127b048e31503483ab382
jwalx <jwalxpatel@gmail.com>
2023-10-18 12:20:02 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from src.MLopsAssignmenr import logger
+from src.MLopsAssignmenr.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
+STAGE_NAME = "Data Ingestation Stage"
 
-logger.info("Welcome to our custom logging")
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
