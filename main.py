5510adc8aa391af336e6863e0d3707bb6594decb
jwalx <jwalxpatel@gmail.com>
2023-10-18 19:17:25 +0530

    data transformation updated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.MLopsAssignmenr import logger
 from src.MLopsAssignmenr.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.MLopsAssignmenr.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-
+from src.MLopsAssignmenr.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestation Stage"
 try:
@@ -24,5 +24,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
    
     
